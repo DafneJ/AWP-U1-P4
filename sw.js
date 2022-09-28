60eae67526885f785f3630ca021bc67f65efb590
DafneJimenezR <20193tn029@utez.edu.mx>
2022-09-27 23:27:53 -0500

    update

--- sw.js
+++ sw.js
@@ -4,15 +4,15 @@ self.addEventListener('install', (event) => {
    const promiseCache= caches.open('cache-v1.1').then((cache) => {
         return cache.addAll(
             [
-                '/',
-                '/index.html',
-                '/images',
-                '/pages/dividir.html',
-                '/pages/suma.html',
-                '/pages/resta.html',
-                '/pages/multiplicacion.html',
-                'css/styles.css',
-                'js/app.js',
+                '/AWP-U1-P4/',
+                '/AWP-U1-P4/index.html',
+                '/AWP-U1-P4/images',
+                '/AWP-U1-P4/pages/dividir.html',
+                '/AWP-U1-P4/pages/suma.html',
+                '/AWP-U1-P4/pages/resta.html',
+                '/AWP-U1-P4/pages/multiplicacion.html',
+                '/AWP-U1-P4/css/styles.css',
+                '/AWP-U1-P4/js/app.js',
                 "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
                 "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
             ]
